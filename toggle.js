5f3bd65be8ca0bcee4ca79b2a63e53cfb48c4928
mattzahel <mateusz.zahel@gmail.com>
2018-04-02 23:08:37 +0200

    Cards toggle

---
+++ toggle.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    const incomesTab = $('#card-incomes').click(function(){
+        $('#incomes-list').toggle(100);
+    })
+    const expensesTab = $('#card-expenses').click(function(){
+        $('#expenses-list').toggle(100);
+    })
+
+})
